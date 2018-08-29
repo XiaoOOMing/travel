b54b5db389ed412a5e699b54334b9782ecf0de2f
XiaoOOMing <13261675306@163.com>
2018-08-29 14:35:36 +0800

    city search ok

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="city-search">
+    <input type="text" placeholder="输入城市名或拼音" class="search-input" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~@style/var.styl"
+  .city-search
+    background $bgClolor
+    height .72rem
+    padding 0 .1rem
+    .search-input
+      box-sizing border-box
+      width 100%
+      padding 0 .1rem
+      height .62rem
+      line-height .62rem
+      text-align center
+      border-radius .06rem
+      color #666
+</style>
